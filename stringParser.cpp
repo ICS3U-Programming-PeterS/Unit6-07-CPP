e9a1ae2fe444b1aed36b1a70098208256b7376a6
Peter Sobowale <peter.sobowale@stu.ocsb.ca>
2023-01-04 01:55:52 +0000

    Added stringParser.cpp

--- stringParser.cpp
+++ stringParser.cpp
@@ -27,29 +27,32 @@ std::list<std::string> ParseString(std::string aString) {
     return words;
 }
 
-// DisplayWords function
-// Accepts a list of words and displays them, one per line
-void DisplayWords(std::list<std::string> words) {
-    for (const std::string& word : words) {
-        std::cout << word << std::endl;
-    }
-}
 
 int main() {
+    // User interface
+    std::cout << "This program will ask the user for a sentence and then"
+              << std::endl;
+    std::cout << "display each word, without spaces, one per line. \n"
+              << std::endl;
+
     // Ask user to enter a sentence
-    std::cout << "Enter a sentence: ";
+    std::cout << "Enter a string: ";
     std::string userInput;
     std::getline(std::cin, userInput);
-    std::cout << "\n";
 
     // Catch any invalid input
     if (userInput.length() == 0) {
         std::cout << "Invalid input. Please try again." << std::endl;
+        return 0;
     }
 
     // Call function to parse the string and return a list of words
     std::list<std::string> words = ParseString(userInput);
 
-    // Call function to display the words, one per line
-    DisplayWords(words);
+    // display words
+    std::cout << "The words in your sentence, without spaces, are:"
+              << std::endl;
+    for (const std::string& word : words) {
+        std::cout << word << std::endl;
+    }
 }
